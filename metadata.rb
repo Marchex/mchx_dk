cc93104c742e2e81a87aa0b0c3c38fbd8177afd8
Chris Nandor <cnandor@marchex.com>
2016-09-09 13:30:24 -0700

    initial installs
    TOOL-446 - install vagrant gems
    TOOL-694 - bootstrap

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,11 @@
 name 'marchefdk'
-maintainer 'Marchex Engineering'
-maintainer_email 'techops@marchex.com'
+maintainer 'Marchex Tools'
+maintainer_email 'tools-team@marchex.com'
 license 'all_rights'
 description 'Installs/Configures marchefdk'
 long_description 'Installs/Configures marchefdk'
-version '0.1.0'
+version '0.1.1'
 
-# Specify version constraint for community cookbooks (not authored by marchex or checked in to https://github.marchex.com/marchex-chef)
-# example:
-# depends 'apt', '=> 2.9.2' # use 'greater than or equal to' version constraint operator unless you have a good reason not to
-
-# DO NOT specify version constraints for marchex-authored cookbooks, just use 'depends' - version control is provided by delivery
-# example:
 depends 'mchx_chef_helpers'
 
 issues_url 'https://jira.marchex.com'
---
+++ spec/unit/recipes/installs_spec.rb
@@ -0,0 +1,66 @@
+#
+# Cookbook Name:: marchefdk
+# Spec:: installs
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+
+require 'spec_helper'
+
+def test_platform(attrs)
+  let(:chef_run) do
+    if attrs.nil?
+      ChefSpec::ServerRunner.new
+    else
+      ChefSpec::SoloRunner.new(attrs)
+    end
+  end
+
+  it 'converges successfully' do
+    chef_run.converge(described_recipe)
+    expect { chef_run }.to_not raise_error
+  end
+
+  it 'installs packages' do
+    chef_run.converge(described_recipe)
+
+    packages = %w(vagrant virtualbox)
+    packages.each do |pkg|
+      expect(chef_run).to install_package pkg
+    end
+  end
+
+  it 'installs gems' do
+    chef_run.converge(described_recipe)
+
+    gems = %w(
+      tty-prompt
+      chef-vault-testfixtures
+      chef-api
+      vagrant-omnibus
+      vagrant-cachier
+    )
+    gems.each do |gem|
+      expect(chef_run).to install_chef_gem gem
+    end
+  end
+end
+
+describe 'marchefdk::installs' do
+  context 'on ubuntu 12.04' do
+    test_platform(platform: 'ubuntu', version: '12.04')
+  end
+
+  context 'on ubuntu 16.04' do
+    test_platform(platform: 'ubuntu', version: '16.04')
+  end
+
+  context 'on centos 6.6' do
+    test_platform(platform: 'centos', version: '6.6')
+  end
+
+  context 'on centos 7.0' do
+    test_platform(platform: 'centos', version: '7.0')
+  end
+end
