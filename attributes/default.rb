536dd71779e50367dc9aabb0577aa64bf8d07b25
Chris Nandor <cnandor@marchex.com>
2016-09-13 14:01:45 -0700

    add inspec tests and clean up a bit

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,13 +10,17 @@ default['marchefdk']['basedir'] = '/site/marchex-chef'
 default['marchefdk']['repo_list'] = {
   'marchex-chef/marchex-chef-generator' => '',
   'marchex-chef/mchx_chef_helpers'      => 'cookbooks',
-  'marchex-chef/marchefdk'              => 'cookbooks'
+  'marchex-chef/marchefdk'              => 'cookbooks',
+  'marchex-chef/tests_mchx_dk'          => 'tests'
 }
 
-default['marchefdk']['package_list'] = %w(
-  vagrant
-  virtualbox
-)
+default['marchefdk']['package_list'] = []
+if node['platform_family'] == 'debian'
+  default['marchefdk']['package_list'] += %w(
+    vagrant
+    virtualbox
+  )
+end
 
 default['marchefdk']['chef_gem_list'] = %w(
   tty-prompt
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ maintainer_email 'tools-team@marchex.com'
 license 'all_rights'
 description 'Installs/Configures marchefdk'
 long_description 'Installs/Configures marchefdk'
-version '0.1.9'
+version '0.1.10'
 
 issues_url 'https://jira.marchex.com'
 source_url 'https://github.marchex.com/marchex-chef/marchefdk'
+
+# we want this to be a standalone cookbook, so no dependencies unless necessary
