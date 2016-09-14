0e4d3ef6b91eaa77715b132557e157ca865d84c3
Chris Nandor <cnandor@marchex.com>
2016-09-14 12:33:07 -0700

    run_cookbook has right path and env

--- run_cookbook.sh
+++ run_cookbook.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "{\"mchx_dk\":{\"basedir\":\"$(cat $HOME/.marchex-chef-basedir)\"}}" | sudo chef-client -z -j /dev/stdin -o 'recipe[mchx_dk::zero]' --disable-config
+
+# make sure we're in the right directory (from marchex-chef/cookbooks/mchx_dk/, should be in marchex-chef)
+cd $(dirname $0)/../..
+
+sudo bash -c '
+    # set correct env
+    eval "$(chef shell-init bash)" &&
+    # pass attributes to chef-client in JSON
+    (echo "{\"mchx_dk\":{\"basedir\":\"$(cat $HOME/.marchex-chef-basedir)\"}}" |
+    # run chef-client in local mode, read JSON attributes, run zero recipe
+    chef-client -z -j /dev/stdin -o recipe[mchx_dk::zero] --disable-config)
+'
