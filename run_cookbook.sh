549022d9bb5e89f4ad475bf3f62e8f43074167f2
Chris Nandor <cnandor@marchex.com>
2016-09-15 11:07:52 -0700

    better chef-client output

--- run_cookbook.sh
+++ run_cookbook.sh
@@ -12,5 +12,5 @@ sudo dk_basedir="$dk_basedir" dk_user="$USER" dk_client_key="$dk_client_key" bas
     # pass attributes to chef-client in JSON
     (echo "{\"mchx_dk\":{\"basedir\":\"$dk_basedir\",\"client_key\":\"$dk_client_key\",\"user\":\"$dk_user\"}}" |
     # run chef-client in local mode, read JSON attributes, run zero recipe
-    chef-client -z -j /dev/stdin -o recipe[mchx_dk::zero] --disable-config)
+    chef-client -z -j /dev/stdin -o recipe[mchx_dk::zero] --disable-config -F min)
 '
