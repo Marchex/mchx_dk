8fb6bf99a9acb0651cfa40b658378db8d88870e9
Chris Nandor <cnandor@marchex.com>
2016-12-13 12:17:11 -0800

    fix script executing in wrong directory sometimes

--- run_cookbook.sh
+++ run_cookbook.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# make sure we're in the right directory (from marchex-chef/cookbooks/mchx_dk/, should be in marchex-chef)
-#cd $(dirname $0)/../..
+set -e
+
 dk_basedir=$(cat $HOME/.marchex-chef-basedir)
 dk_user=$USER
 dk_client_key=$(cat $HOME/.marchex-chef-client-key)
 
+cd "$dk_basedir/cookbooks/mchx_dk"
 sudo rm -rf cookbooks
 
 sudo dk_basedir="$dk_basedir" dk_user="$USER" dk_client_key="$dk_client_key" bash -c '
