195ff7bb44c7ebd8ecfc1679869ab2f40dd18b00
Chris Nandor <cnandor@marchex.com>
2017-01-23 08:38:23 -0800

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tools-team@marchex.com'
 license 'all_rights'
 description 'Installs/Configures mchx_dk'
 long_description 'Installs/Configures mchx_dk'
-version '0.1.40'
+version '0.1.41'
 
 issues_url 'https://jira.marchex.com'
 source_url 'https://github.marchex.com/marchex-chef/mchx_dk'
